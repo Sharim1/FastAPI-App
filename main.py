58b435958bbadc7bc2905c225658140ac97f0b15
sharim raza <sarimpervez@gmail.com>
2022-04-02 23:30:22 +0530

    Added logging using Azure Log Handler

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ import utils
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import logging
+from opencensus.ext.azure.log_exporter import AzureLogHandler
 
 
 dotenv_path = Path(__file__).resolve().parent / 'fastapi.env'
 load_dotenv(dotenv_path=dotenv_path)
 
 app = FastAPI()
+
+logger = logging.getLogger(__name__)
+logger.setLevel(10)
+logger.addHandler(AzureLogHandler(connection_string="InstrumentationKey={}".format(os.getenv('INSTRUMENTATIONKEY'))))
 
 headers = {
     "Ocp-Apim-Subscription-Key": os.getenv('APIKEY'),
@@ -29,6 +35,16 @@ def analyze_text(text: Model):
 
         sentiment = utils.call_text_analytics_api(headers, document, endpoint='sentiment')
         keyphrases = utils.call_text_analytics_api(headers, document, endpoint='keyPhrases')
+
+        log_data = {
+            "custom_dimensions":
+            {
+                "text": text.text_to_analyze[i],
+                "text_sentiment": sentiment["documents"][0]["sentiment"],
+                "text_keyphrases": keyphrases["documents"][0]["keyPhrases"]
+            }
+        }
+        logger.info('Text Processed Succesfully', extra=log_data)
 
         response["sentiment"].append(sentiment["documents"][0])
         response["keyphrases"].append(keyphrases["documents"][0])
